bd9b7844397113414c71502482bdaaeb5c5ace5f
Vivek Viradia <vivekviradia123@gmail.com>
2023-06-12 11:59:08 +0530

    file Restructure & Resturant Component

---
+++ src/component/Basics/Resturant.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./style.css"
+
+const Resturant = () => {
+  return (
+    <React.Fragment>
+      <div className='card-container'>
+        <div className='card'>
+          <div className='card-body'>
+            <span className='card-number card-circle subtitle'>1</span>
+            <span className='card-author subtle'>
+              BreakFast
+            </span>
+            <h2 className='card-title'>Maggie</h2>
+            <span className='card-description subtle'>I love Maggie realy gkojn gkgnfg gdigkdf giognwgowt wgrghiof sf f9jfadf  fsdifhsdgksg sgioj </span>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  )
+}
+
+export default Resturant
