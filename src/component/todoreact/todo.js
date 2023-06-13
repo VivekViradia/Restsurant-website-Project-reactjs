e453ff54b6be0f774a8f53e21884fef18728b5dd
Vivek Viradia <vivekviradia123@gmail.com>
2023-06-13 18:30:28 +0530

    TODO List Completed

--- src/component/todoreact/todo.js
+++ src/component/todoreact/todo.js
@@ -1,14 +1,39 @@
 import React, { useEffect, useState } from 'react'
 import "./style.css"
 
+// Get The LocalStorage Data back
+const getLocalData = () => {
+    const lists = localStorage.getItem("mytodolist")
+    if (lists) {
+        return JSON.parse(lists);
+    } else {
+        return []
+    }
+}
+
 function Todo() {
 
     const [inputData, setInputData] = useState('')
-    const [items, setItems] = useState([])
-    //ADd the item funstions
+    const [editItem, setEditItem] = useState('')
+    const [toggelButton, setToggelButton] = useState(false)
+    const [items, setItems] = useState(getLocalData())
+
+    //Add the item functions
     const addItem = () => {
         if (!inputData) {
             alert('Plz fill the data📝')
+        } else if (inputData && toggelButton) {
+            setItems(
+                items.map((curElem) => {
+                    if (curElem.id === editItem) {
+                        return { ...curElem, name: inputData }
+                    }
+                    return curElem
+                })
+            )
+            setInputData([])
+            setEditItem(null)
+            setToggelButton(false)
         } else {
             const newInputData = {
                 id: new Date().getTime().toString(),
@@ -19,6 +44,16 @@ function Todo() {
         }
     }
 
+    // Update the items
+    const editItems = (index) => {
+        const item_todo_edited = items.find((curElem) => {
+            return curElem.id === index;
+        })
+        setInputData(item_todo_edited.name)
+        setEditItem(index)
+        setToggelButton(true)
+    }
+
     // Delete item
     const deleteItem = (index) => {
         const updatedItem = items.filter((curElem) => {
@@ -27,9 +62,10 @@ function Todo() {
         setItems(updatedItem)
     }
 
-    useEffect(() => [
-        localStorage.setItem("mytodolist", JSON.stringify(items))
-    ], [items])
+    // adding localStorage
+    useEffect(() => {
+        localStorage.setItem("mytodolist", JSON.stringify(items));
+    }, [items]);
 
     return (
         <>
@@ -41,7 +77,9 @@ function Todo() {
                     </figure>
                     <div className='addItems'>
                         <input type='text' className='form-control' placeholder=" Add Item✍" value={inputData} onChange={(event) => setInputData(event.target.value)} />
-                        <i className='fa fa-plus add-btn' onClick={addItem}></i>
+                        {
+                            toggelButton ? (<i className='fa fa-edit add-btn' onClick={addItem}></i>) : (<i className='fa fa-plus add-btn' onClick={addItem}></i>)
+                        }
                     </div>
                     <div className='showItems'>
                         {/* show our items */}
@@ -52,7 +90,7 @@ function Todo() {
                                         <div className='eachItem' key={curElem.id}>
                                             <h3>{curElem.name}</h3>
                                             <div className='todo-btn'>
-                                                <i className='far fa-edit add-btn' ></i>
+                                                <i className='far fa-edit add-btn' onClick={() => editItems(curElem.id)}></i>
                                                 <i className='far fa-trash-alt add-btn' onClick={() => deleteItem(curElem.id)}></i>
                                             </div>
                                         </div>
@@ -60,7 +98,7 @@ function Todo() {
                                 })
                             }
                         </div>
-                        <button className='btn effect04' data-sm-link-text='Remove All' onClick={setItems([])}>
+                        <button className='btn effect04' data-sm-link-text='Remove All' onClick={() => setItems([])}>
                             <span> CHECK LIST</span>
                         </button>
                     </div>
